348a4948d88bf275c7d8a75b99c73c23cccc0691
Varsha Ravindra <varsharavindra12@gmail.com>
2018-02-06 17:04:37 +0530

    Create restaurant.sql

---
+++ SQL commands/restaurant.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `restaurant` (
+  `id` int(11) NOT NULL,  `name` varchar(30), 
+ `address` varchar(30),   `city` varchar(30),  `ratings` varchar(5) );
+
+ALTER TABLE `restaurant`  ADD PRIMARY KEY (`id`);
